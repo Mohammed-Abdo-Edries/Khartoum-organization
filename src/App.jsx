c56fc4d087d933b2228103af5dd783db8538db28
Mohammed-Abdo-Edries <mohammed.abdo1916@gmail.com>
2024-01-21 10:24:45 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './Components/Navbar'
+import './index.css'
+
+function App() {
+  return (
+    <>
+    <Navbar />
+      <div className="">
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            منظمة الخرطوم للدعاية والاعلان
+        </div>
+    )
+}
+
+export default Navbar
